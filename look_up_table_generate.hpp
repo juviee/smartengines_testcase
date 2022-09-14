9ad2766c84f0ac8161fed8b089fe8ed5f349ac8d
bayazit <b_umutbaev@proton.me>
2022-09-14 08:34:34 +0300

    lut chgs

--- look_up_table_generate.hpp
+++ look_up_table_generate.hpp
@@ -7,10 +7,27 @@ namespace fastexp
 template <std::size_t N, typename F = std::identity>
 constexpr auto gen_float_array_exp_step(const F& f = F{}) 
 {
-   std::array<float, N> arr;
-   for (size_t i = 0; i < N; ++i)
-      arr[i] = f(static_cast<size_t>(1<<i));
-   return arr;
+    std::array<float, N+1> arr;
+    arr[0] = f(static_cast<size_t>(0));
+    for (size_t i = 0; i < N; ++i)
+    {
+        arr[i+1] = f(static_cast<size_t>(1<<i));
+    }
+    return arr;
+}
+
+template <std::size_t N, typename F = std::identity>
+constexpr auto gen_float_arr_lookup_exp_step(const F& f = F{}) 
+{
+    std::array<float, N+1> arr;
+    arr[0] = f(static_cast<size_t>(0), static_cast<size_t>(1));
+    for (size_t i = 0; i < N; ++i)
+    {
+        arr[i+1] = f(static_cast<int64_t>(1<<i),
+                     static_cast<int64_t>(1<<(i+1))
+                    );
+    }
+    return arr;
 }
 }; // namespace fastexp
 #endif
